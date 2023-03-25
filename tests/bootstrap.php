52a8749897e2658fe334f531328391311c7e55a2
Marcin Morawski <marcin@morawskim.pl>
2023-03-25 09:44:55 +0100

    Use different version of composer-test-scenarios.
    Ensure composer autoload fix works

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,5 @@
 <?php
 
 $loader = require __DIR__.'/../vendor/autoload.php';
-$loader->addPsr4('Mmo\RequestCollector\\', __DIR__ . '/../tests/Unit/');
-$loader->addPsr4('Mmo\RequestCollector\\', __DIR__ . '/../tests/Acceptance/');
+//$loader->addPsr4('Mmo\RequestCollector\\', __DIR__ . '/../tests/Unit/');
+//$loader->addPsr4('Mmo\RequestCollector\\', __DIR__ . '/../tests/Acceptance/');
